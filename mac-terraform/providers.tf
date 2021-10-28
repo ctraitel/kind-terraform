5e0d68a961b339a72080bbaa19d54655c938f2e7
ctraitel <c.traitel01@gmail.com>
2021-10-28 14:57:46 -0400

    moving terraform script to mac folder

---
+++ mac-terraform/providers.tf
@@ -0,0 +1,20 @@
+provider "kubernetes" {
+  config_path    = local_file.kube_config.filename
+  config_context = "kind-test-cluster"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path    = local_file.kube_config.filename
+    config_context = "kind-test-cluster"
+  }
+}
+
+provider "kind" {}
+
+provider "docker" {}
+
+provider "kubectl" {
+  config_path    = local_file.kube_config.filename
+  config_context = "kind-test-cluster"
+}
